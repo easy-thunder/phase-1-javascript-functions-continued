e46d5073b44355692d707087d66ad3b95a7ed5bf
Jacob David Diehl <jakediehl17@gmail.com>
2022-12-12 15:15:39 -0700

    commit tree 492d59769bb56f899acb746a654e34e47aa87bbb

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // code your solution here
+const saturdayFun = function(fun="roller-skate") {
+    return `This Saturday, I want to ${fun}!`
+};
+
+
+const mondayWork = function(work='go to the office'){
+    return `This Monday, I will ${work}.`
+};
+
+
+
+const wrapAdjective = function(adjective){
+    return function (message){
+        return `You are ${adjective}${message}${adjective}!`;
+    };
+
+}
+
+
+
+
+
+// console.log(wrapAdjective('%')("a dedicated programmer") );
+// "you are %a dedicated programmer%!"
+
+
+
+
+
+
